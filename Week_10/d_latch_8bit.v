bf05e8521c53366dd1a473619a60fba28cc343e0
emmettmallon <156140491+emmettmallon@users.noreply.github.com>
2025-10-28 11:35:13 -0600

    Add 8-bit D latch module
    Implement an 8-bit D latch module with generate block.

---
+++ Week_10/d_latch_8bit.v
@@ -0,0 +1,19 @@
+module d_latch_8bit (
+    input  [7:0]D,
+    input  E,
+    output reg[7:0] Q
+);
+   
+    genvar i;
+    generate    
+        for (i = 0; i < 8; i = i + 1) begin
+          d_latch_8bit latch( // some error on this line
+            .D (D[i]),
+            .E (E),
+            .Q (Q[i])
+         );
+         end
+         
+       endgenerate
+         
+endmodule
